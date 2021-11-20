f24fc297da5616e4a2cf690384ef22095835bcd9
YeChanSong <davidsyc1170@gmail.com>
2021-11-20 23:28:01 +0900

    예방접종 index화면 및 부트스트랩 추가
    부트스트랩 코드는 public에 위치시켜 두었습니다.

--- 백신_접종_예약_시스템/routes/index.js
+++ 백신_접종_예약_시스템/routes/index.js
@@ -1,25 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit: 5,
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'DBproject'
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-    // use connection
-    connection.query('select * from USERS', function(err, rows){
-      if(err) console.error("err: "+err);
-      console.log("rows : " + JSON.stringify(rows));
-      res.render('index', {title: 'test', rows: rows});
-      connection.release();
-    });
-  });
+  res.render('index', {title: '예방접종 사전예약 시스템'});
 });
 
 module.exports = router;
