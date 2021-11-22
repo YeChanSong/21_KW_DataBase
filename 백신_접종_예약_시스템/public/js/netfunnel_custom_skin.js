0e605c676260ca8d142f5b1d930a114ee7fbb45e
David <54590290+YeChanSong@users.noreply.github.com>
2021-11-22 23:33:38 +0900

    인코딩 처리

--- 백신_접종_예약_시스템/public/js/netfunnel_custom_skin.js
+++ 백신_접종_예약_시스템/public/js/netfunnel_custom_skin.js
@@ -6,17 +6,17 @@ if(typeof NetFunnel == "object") {
             var custom_timeLeft = document.getElementById("Custom_TimeLeft");
             var timeLeftStr = "";
             if(timeleft > 1800) {
-                timeLeftStr = "30遺� �댁긽 �덉긽�⑸땲��.";
+                timeLeftStr = "30분 이상 예상됩니다.";
             } else if(timeleft > 600) {
-                timeLeftStr = "10遺� �댁긽 �덉긽�⑸땲��.";
+                timeLeftStr = "10분 이상 예상됩니다.";
             } else {
                 var min = parseInt((timeleft % 3600) / 60);
                 var sec = timeleft % 60;
 
                 if(min == 0) {
-                    timeLeftStr = sec + "珥� �덉긽�⑸땲��."
+                    timeLeftStr = sec + "초 예상됩니다."
                 } else {
-                    timeLeftStr = min + "遺� " + sec + "珥� �덉긽�⑸땲��."
+                    timeLeftStr = min + "분 " + sec + "초 예상됩니다."
                 }
             }
 
@@ -25,11 +25,11 @@ if(typeof NetFunnel == "object") {
         htmlStr:
             '<div class="main-ready" id="NetFunnel_Skin_Top" style="background-color: #f0f2f4; width:900px; height: 500px; padding: 30px; margin-top: 50px; border: 3px solid dimgray;">' +
             '<div class="main-visual"><div class="head-visual">' +
-            '<h2><strong class="-mr-3" style="margin: 0 !important;">蹂몄씤 �몄쬆</strong>�� �꾪빐 �쒕퉬�� �묒냽 ��湲곗쨷 �낅땲��.<br/>�좎떆留� 湲곕떎�� 二쇱꽭��.</h2></div></div>' +
-            '<p class="waiting-time" style="font-size: 1.5rem; text-align: right; font-weight: 500; padding-top: 3.5rem;">�덉긽 ��湲� �쒓컙 : <span id="Custom_TimeLeft"></p>' +
+            '<h2><strong class="-mr-3" style="margin: 0 !important;">본인 인증</strong>을 위해 서비스 접속 대기중 입니다.<br/>잠시만 기다려 주세요.</h2></div></div>' +
+            '<p class="waiting-time" style="font-size: 1.5rem; text-align: right; font-weight: 500; padding-top: 3.5rem;">예상 대기 시간 : <span id="Custom_TimeLeft"></p>' +
             '<div class="progress-bar" style="width: 840px; max-width: 840px; height: 30px; max-height: fit-content;" id="NetFunnel_Loading_Popup_Progressbar"></div>' +
-            '<br/><p style="margin-top: 25px;">�꾩옱 蹂몄씤 �몄쬆 �붿껌 �섍� 留롮븘 ��湲� 以묒씠硫�, �좎떆留� 湲곕떎由ъ떆硫� �좏깮�섏떊 �몄쬆 �쒕퉬�ㅻ줈 �먮룞 �묒냽�⑸땲��.</p><br/>' +
-            // '<p class="progress-caution">�� �ъ젒�랁븯�쒕㈃ ��湲� �쒓컙�� �� 湲몄뼱吏묐땲��.</p>' +
+            '<br/><p style="margin-top: 25px;">현재 본인 인증 요청 수가 많아 대기 중이며, 잠시만 기다리시면 선택하신 인증 서비스로 자동 접속됩니다.</p><br/>' +
+            // '<p class="progress-caution">※ 재접속하시면 대기 시간이 더 길어집니다.</p>' +
             '</div>'
     },'normal');
 }
