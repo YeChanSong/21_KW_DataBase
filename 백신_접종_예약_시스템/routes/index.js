b423b1874524cc84c12aabebb91fe7aade5ee6f2
YeChanSong <davidsyc1170@gmail.com>
2021-11-24 01:39:15 +0900

    네이버 프로필 조회 기능 추가
    UI 미구현, 가져온 정보를 바탕으로 나이 계산, 주민번호는 가져오지 않으므로 고유ID 활용하는 방법은 어떨지 논의.
    카카오 로그인 기능 구현 예정

--- 백신_접종_예약_시스템/routes/index.js
+++ 백신_접종_예약_시스템/routes/index.js
@@ -23,6 +23,8 @@ var client_secret = 'FWSYT96uK5';
 var state = "G";
 var redirectURI = encodeURI("http://localhost:3000/Api/Member/Oauth2ClientCallback");
 var api_url = "";
+var token = "";
+var tokenparser;
 router.get('/naverlogin', function (req, res) {
   api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
@@ -38,10 +40,16 @@ router.get('/Api/Member/Oauth2ClientCallback', function (req, res) {
         url: api_url,
         headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
      };
+     
     request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-        res.end(body);
+        // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        // res.end(body);
+        tokenparser = JSON.parse(body);
+        token = tokenparser.access_token;
+        console.log(token);
+        
+        res.redirect('/member');
       } else {
         res.status(response.statusCode).end();
         console.log('error = ' + response.statusCode);
@@ -54,5 +62,37 @@ router.get('/Api/Member/Oauth2ClientCallback', function (req, res) {
   // });
 
 
+  /* 회원 프로필 조회 */
+  // 고유ID, 생년월일, 이름, 전화번호를 가져온다
+  // 생년월일 --> 나이 계산 // 주민등록번호 대신 고유ID를 활용.. --> 고유ID의 길이가 20을 넘으므로 USERS 테이블 수정해야 할 수 있다.
+  router.get('/member', function (req, res) {
+    var header = "Bearer " + token; // Bearer 다음에 공백 추가
+    console.log(header);
+    console.log(token);
+     var api_url = 'https://openapi.naver.com/v1/nid/me';
+     var request = require('request');
+     var options = {
+         url: api_url,
+         headers: {'Authorization': header}
+      };
+      
+     request.get(options, function (error, response, body) {
+       
+       if (!error && response.statusCode == 200) {
+         res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+         res.end(body);
+       } else {
+         console.log('error');
+         if(response != null) {
+           res.status(response.statusCode).end();
+           console.log('error = ' + response.statusCode);
+         }
+       }
+     });
+   });
+  //  app.listen(3000, function () {
+  //    console.log('http://127.0.0.1:3000/member app listening on port 3000!');
+  //  });
+  
 
 module.exports = router;
