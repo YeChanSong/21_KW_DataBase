4aa7219102834860a893a890266c90e232ec7541
YeChanSong <davidsyc1170@gmail.com>
2021-11-24 15:08:47 +0900

    naverlogin 인증화면 작업 ver1

--- 백신_접종_예약_시스템/routes/index.js
+++ 백신_접종_예약_시스템/routes/index.js
@@ -27,9 +27,11 @@ var token = "";
 var tokenparser;
 router.get('/naverlogin', function (req, res) {
   api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
-   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-   res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+  //  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+  //  res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+  res.render('naverlogin', {title: '예방접종 사전예약 시스템'});
  });
+
 router.get('/Api/Member/Oauth2ClientCallback', function (req, res) {
     code = req.query.code;
     state = req.query.state;
