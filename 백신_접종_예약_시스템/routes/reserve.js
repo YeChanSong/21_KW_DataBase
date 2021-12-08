e5f54fc18fff667f397944d15a07871d00b7f8fe
arsgsg1 <arsgsg1@gmail.com>
2021-12-08 16:14:15 +0900

    코드 정리

--- 백신_접종_예약_시스템/routes/reserve.js
+++ 백신_접종_예약_시스템/routes/reserve.js
@@ -1,25 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
 const passport = require('passport');
-var pool = mysql.createPool({
-    connectionLimit: 5,
-    host: 'localhost',
-    user: 'root',
-    port: 33060,
-    password: '1234',
-    database: 'dbproject'
-});
-
-let InjectPolicy = function(){
-    pool.getConnection((err, connection) => {
-       let query = `SELECT COUNT(*) vaccine_reservation WHERE reservation_date = ?`;
-       connection.query(query, (err, rows) => {
-           connection.release();
-           return rows[0] <= 10;
-       });
-    });
-};
+var pool = require('./db')
 
 //접종 가능 병원 조회 API
 /*
@@ -38,74 +20,14 @@ router.get('/', function(req, res, next){
     });
 });
 
-//백신 접종 완료 api
 /*
-*   hospital_id: 병원 id
-*   vaccinated_number: 접종 차수
-*   user_id: 유저의 id
-*  */
-router.post('/user/:uid', function(req, res, next){
-   //유저의 접종 차수 + 1
-    pool.getConnection(function(err, connection){
-       let query =
-           `
-                UPDATE users
-                SET Vaccinated_Number = ?
-                WHERE User_number = ?
-           `;
-       connection.query(query, [req.body.vaccinated_number + 1, req.params.uid], function(err, rows){
-           connection.release();
-       });
-   });
-    //예약 내역 지우기
-    pool.getConnection(function(err, connection){
-        let query =
-            `
-                DELETE FROM vaccine_reservation
-                WHERE user_number = ?
-           `;
-        connection.query(query, [req.params.uid], function(err, rows){
-            connection.release();
-        });
-    });
-});
-//병원에 예약된 내역
-router.get('/hospital/:id', function(req, res, next){
-   pool.getConnection(function (err, connection){
-       let query =
-           `
-                SELECT * FROM vaccine_reservation v 
-                JOIN hospitals h ON h.hospital_id = ?
-                JOIN users u ON u.User_number = v.user_number
-           `;
-       connection.query(query, [req.params.id], function(err, rows){
-          if(err) next(err);
-          else{
-              res.json(rows);
-          }
-          connection.release();
-       });
-   });
-});
-
-//유저의 예약 내역
-router.get('/user/:uid', function(req, res, next){
-    pool.getConnection(function (err, connection){
-        let query =
-            `
-                SELECT * FROM vaccine_reservation v 
-                JOIN users u ON u.User_number = ?
-                JOIN hospital h ON h.hospital_id = u.hospital_id
-            `;
-        connection.query(query, [req.params.uid], function(err, rows){
-            if(err) next(err);
-
-            else{
-                res.render('/reservation/confirm', {rows});
-            }
-            connection.release();
-        });
-    });
+* 병원 form 뿌려주는 API
+* */
+router.get('/:hid', (req, res, next) => {
+    res.locals.stuff={
+        hid: req.params.hid
+    }
+    res.render('reservation/request');
 });
 
 //예약 API
@@ -118,26 +40,24 @@ router.get('/user/:uid', function(req, res, next){
 * NOTE: 10개를 초과하는 예약은 불가능하게 동작
 * */
 router.post('/', function(req, res, next){
-   if(!InjectPolicy()){
-       res.render('/reservation', {'message': '예약 개수가 초과되어 더 이상 예약할 수 없는 병원입니다.'});
-       return;
-   }
-    const b = req.body;
-    pool.getConnection((err, connection) => {
-      let query =
-      `
-        INSERT INTO vaccine_reservation (hospital_id, reservation_date, vaccine_id, inoculation_number, user_number)
+    console.log(req.body);
+    const query =
+        `
+        INSERT INTO VACCINE_RESERVATION 
+        (hospital_id, reservation_date, vaccine_id, inoculation_number, user_number) 
         VALUES(?, ?, ?, ?, ?)
-      `;
-      connection.query(query, [b.hospital_id, b.reservation_date, b.vaccine_id, b.inoculation_number, b.user_number],
-          (err, rows) => {
-            if(err) next(err);
-            else{
-                //등록 후에는 리다이렉트
-                res.redirect('/reservation');
-            }
-          });
-   });
+        `;
+    pool.getConnection((err, connection)=>{
+        const b = req.body;
+        const params = [b.hospital_id, b.reservation_date, b.vaccine_id, 1, b.id];
+       connection.query(query, params, (err, rows) => {
+           if(err){
+               console.log('[ERR] post /' + err);
+           }
+           connection.release();
+           res.redirect('/reservation');
+       }) ;
+    });
 });
 
 //예약 취소 API
